4e3c407bc5f607660fc050413e47e86922d56e80
petko.h.bankov <petko.h.bankov@gmail.com>
2020-03-19 10:50:22 +0200

    this was done because video started playing while display was = to none

--- javascript/watchVideoBtn.js
+++ javascript/watchVideoBtn.js
@@ -1,9 +1,21 @@
 let UIv_header = document.querySelector(".v-header");
-let UIoverlay = document.querySelector(".overlay");
 let watchVideoBtn = document.getElementById("watch_video");
 watchVideoBtn.addEventListener("mousedown", e => {
+  let UIdiv = document.createElement("div");
+  UIdiv.classname = "overlay";
+  UIdiv.innerHTML = `
+  <div class="overlay-video-wrap">
+        <iframe
+          src="https://www.youtube.com/embed/O6ED5BleV_w?autoplay=1&start=4&rel=0"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        ></iframe>
+      </div>
+  `;
+  UIv_header.parentNode.insertBefore(UIdiv, UIv_header);
   UIv_header.style.display = "none";
-  UIoverlay.style.display = "flex";
+  UIdiv.style.display = "flex";
   console.log("btn pressed");
   e.preventDefault();
 });
