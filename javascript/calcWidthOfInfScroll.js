e00a5685cdb84282e9f84e094a86f8118cf63f03
petko.h.bankov <petko.h.bankov@gmail.com>
2020-03-28 21:43:43 +0200

    Edge fix

--- javascript/calcWidthOfInfScroll.js
+++ javascript/calcWidthOfInfScroll.js
@@ -16,11 +16,17 @@ document.querySelector("#keyframes").innerHTML = `@keyframes teamScroll {
   0% {
     -webkit-transform: translateX(0);
     transform: translateX(0);
+    -moz-transform:translateX(0);
+    -o-transform:translateX(0);
+    -ms-transform:translateX(0);
   }
 
   100% {
     -webkit-transform: translateX(${sectionTeamWidth}px);
     transform: translateX(${sectionTeamWidth}px);
+    -moz-transform: translateX(${sectionTeamWidth}px);
+    -o-transform: translateX(${sectionTeamWidth}px);
+    -ms-transform: translateX(${sectionTeamWidth}px);
   }
 }`;
 
